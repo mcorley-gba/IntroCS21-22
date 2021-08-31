f0dc976f761b369f901f09f2c222db8847d80a0f
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-08-31 08:30:04 -0400

    Create intro_strings.py

---
+++ homework/intro_strings.py
@@ -0,0 +1,34 @@
+#Write Code for each problem below. Start your code for each problem below the comment
+#at the start each problem. When you have finished, re-save this script as 
+#[yourname]_intro_strings.py and upload to Google Classroom in the assignment.
+
+#Problem 1: 
+#Store a person's name in a variable. Print a simple message to that person.
+#Example: Hello, Joe. Nice weather today.
+
+
+
+
+#Problem 2:
+#Use the same name variable as Problem 1. Print the person's name in 
+#uppercase, lowercase, and title case. Use the appropriate command for each,
+#regardless of how you have your name entered.
+#Example: JOE, joe, and Joe
+
+
+
+
+#Problem3: 
+#Use the same name variable as before. Add some whitespace characters at the
+#beginning and end of the name (use each of the commands \t and \n at least once).
+#Print the name three times using the functions lstrip(), rstrip(), and strip(),
+#respectively. 
+
+
+
+
+#Problem 4:
+#Find a quotation from a famous person you admire. Print the quotation and the name
+#of the person. Your printed quotation should include quotation marks.
+#Example: George H. W. Bush once said, "Read my lips: No New Taxes". 
+
