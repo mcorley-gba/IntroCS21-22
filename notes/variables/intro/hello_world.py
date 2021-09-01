adc3454ab3b372f206f861e08429dc9fcd6d7026
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-09-01 09:47:55 -0400

    Class notes for Sept 1, 2021

--- notes/variables/intro/hello_world.py
+++ notes/variables/intro/hello_world.py
@@ -1,1 +1,40 @@
-print("Hello, world!")
+message = "Hello, world!"
+
+print(message)
+
+message = "Hello, all y'all!"
+
+print(message)
+
+#Rules for Naming Variables
+# 1. Variable naems can only contain: letters, numbers, and underscores 
+# 2. Variables can start with letters or underscores, but NOT numbers.
+#       Ex: _message_1 - Good!
+#           message_1 - Good!
+#           1st_message - BAD!
+# 3. Spaces are NOT allowed. 
+#       Ex: hello_message - Good!
+#           hello message - BAD!
+# 4. Avoid using built-in keywords and functions
+#       Ex: print, input, eval, turtle
+# 5. Variable names should be short, but descriptive
+#       Ex: n   vs.     name
+#       Ex: s_n vs.     student_name
+#       Ex: length_of_this_student_name     vs. name_length
+# 6. Be careful with l, 1, O, and 0
+# 7. For now, avoid starting with uppercase letters.
+
+# Two Common Naming Conventions:
+# 1. underscores --> Ex: name_length
+# 2. Camel       --> Ex: nameLengthOfStudent
+# 3. Your own personal preference -- IMPORTANT: Be Consistent
+
+#Typos
+mesage = "hello"
+print(mesage)
+
+#What exactly are variables?
+#Common idea --> variables are boxes in which we can store things
+#                *slightly inaccurate (esp. later on)
+#Better --> Variables are 'labels', or 'nicknames', for other (often
+#           more complicated) things.
