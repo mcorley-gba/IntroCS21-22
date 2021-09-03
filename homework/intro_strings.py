93cca9b479a03a4e6b8ac3a5420276a5da1b855a
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-09-03 12:11:34 -0400

    Clarified Directions and added formatting.

--- homework/intro_strings.py
+++ homework/intro_strings.py
@@ -1,34 +1,39 @@
 #Write Code for each problem below. Start your code for each problem below the comment
-#at the start each problem. When you have finished, ensure this script is saved as 
-#[yourname]_intro_strings.py and upload to Google Classroom in the assignment.
+#   at the start each problem. When you have finished, ensure this script is saved as 
+#   [yourname]_intro_strings.py and upload to Google Classroom in the assignment.
 
 #Problem 1:
-#Store a person's name in a variable. Print a simple message to that person.
-#Example: Hello, Joe. Nice weather today.
+#   Store a person's name in a variable. Print a simple message to that person.
+#   Example: Hello, Joe. Nice weather today.
+#   Write your code below:
 
 
 
 
 #Problem 2:
-#Use the same name variable as in Problem 1. Print the person's name in 
-#uppercase, lowercase, and title case. Use the appropriate command for each,
-#regardless of how you have the name entered.
-#Example: JOE, joe, and Joe
+#   Use the same name variable as in Problem 1. Print the person's name in 
+#   uppercase, lowercase, and title case. Use the appropriate command for each,
+#   regardless of how you have the name entered.
+#   Example: JOE, joe, and Joe
+#   Write your code below:
 
 
 
 
 #Problem3: 
 #Use the same name variable as Problems 1 and 2. Add some whitespace characters at 
-#the beginning and end of the name (use each of the commands \t and \n at least once).
-#Print the name three times using the functions lstrip(), rstrip(), and strip(),
-#respectively. 
+#   the beginning and end of the name (use each of the commands \t and \n at least once).
+#   Print the name four times: once with all the whitespace you added, then three more times
+#   using the functions lstrip(), rstrip(), and strip(), respectively. 
+#   Write your code below:
 
 
 
 
 #Problem 4:
-#Find a quotation from a famous person you admire. Print the quotation and the name
-#of the person. Your printed quotation should include quotation marks.
-#Example: George H. W. Bush once said, "Read my lips: No New Taxes". 
+#   Find a quotation from a famous person you admire. Print the quotation and the name
+#   of the person. Your printed quotation should include quotation marks.
+#   Example: George H. W. Bush once said, "Read my lips: No New Taxes". 
+#   Write your code below:
+
 
