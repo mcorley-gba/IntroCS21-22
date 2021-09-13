eb4a72eb14956d84001ed373ceadb2f406bdf198
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-09-13 14:48:56 -0400

    removed unnecessary files

--- notes/test.py
+++
@@ -1,5 +0,0 @@
-operation = input("What operation to perform?")
-number_1 = input("What is the first number?")
-number_2 = input("What is the second number?")
-
-print(number_1+operation+number_2, "=", eval(number_1+operation+number_2))
