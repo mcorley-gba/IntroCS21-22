755b7c9ad38fcdcd1a79f0d9ae7148ab803b842f
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-09-13 04:58:51 -0400

    Added Questions

---
+++ tests/test1.py
@@ -0,0 +1,29 @@
+#This is Part 2 of test1. Below there are two programming problems
+#   Program original code for each problem. You may use your notes, but
+#   you may not use the internet or your neighbor.
+
+#Problem 1:
+#   Find a quotation from a famous person (you may use the internet
+#   for this). Store the name of the person in a variable. Store the 
+#   quotation in a separate variable. Print your quotation and person
+#   by using the variables rather than the direct information. Use proper 
+#   quotation marks for your quotation.
+#   Example: John F. Kennedy said, "Ask not what your country can do for you, 
+#               but what you can do for your country"
+#Write your code below:
+
+
+
+
+
+
+
+
+#Problem 2:
+#   Use the built-in math capabilities of python and write 
+#   six arithmetic problems that equal the number 8.
+#   Each problem should use one of the five basic arithmetic 
+#   operations: Addition, Subtraction, Multiplication, Division,
+#   Integer Division, and Exponentiation. Print each 
+#   arithmetic problem.
+#Write your code below.
