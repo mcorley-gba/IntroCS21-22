7d7e610d7c8c4aa92dedfa0dea0bfb35e1008390
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-09-20 09:10:58 -0400

    New HW: list_hw1

---
+++ homework/list_hw1.py
@@ -0,0 +1,41 @@
+#Problem1: 
+#   If you could invite anyone, living or deceasedk to a dinner party,
+#   who would you invite? Make a list that includes at least 3 people
+#   you'd like to invite to dinner. Then use the list to print a message
+#   to each person, inviting them to dinner.
+#Write your code below:
+
+
+
+
+
+#Problem2: 
+#   You just found out that one of your dinner guests can't make it. 
+#   So you need to send out new invitations. You must think of another
+#   person to invite.
+#   First, start with your code from problem 1 (copy/paste below). Add 
+#       a print() call at the end of stating the name of the guest who cannot
+#       make it to dinner.
+#   Second, modify the list of guests using appropriate python tools
+#       to replace the guest who cannot make it with your new invitee.
+#   Third, print a second list of invitation messages, one for each person
+#       who is still on your list.
+#Write your code below:
+
+
+
+
+
+#Problem3:
+#   You just found a bigger dinner table, so now more space is available.
+#   Think of three more guests to invite.
+#   First, write a print statement informing everyone that you found a
+#       bigger table.
+#   Second, use the insert() method to add one new guest to the beginning
+#       of your guest list.
+#   Third, use the insert() method to add one new guest to the middle of
+#       of your list.
+#   Fourth, use the append() method to add one new guest to the end of 
+#       our list.
+#   Fifth, print a new set of invitations, one for each guest.
+#Write your code below:
