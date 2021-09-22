b5e94529ee49011652aff5afe5c9e3a1d7c59812
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-09-21 21:10:45 -0400

    list hw2 added

---
+++ homework/lists_hw2.py
@@ -0,0 +1,23 @@
+#Organizing Lists:
+
+#Problem1:
+#   Think of at least five places in the world you would like to visit.
+#   1. Store the locations in a list. Make sure the list is NOT alphabetized
+#   2. Print your list in the original order.
+#   3. Use sorted() to print your list in alphabetical order without modifying
+#       the actual list.
+#   4. Show that your list is still in original order by printing it.
+#   5. Use sorted to print your list in reverse alphabetical order without
+#       permanently changing the list order.
+#   6. Show that your list is still in original order by printing it again.
+#   7. Use reverse() to change the order of your list. Print your list to show
+#       that its order has changed.
+#   8. Use reverse() to change the order of your list again. Print the list to 
+#       show that it is back to the original order.
+#   9. Use sort() to permanently change your list to be in alphabetical order. 
+#       Print the list to show what has been changed.
+#   10. Use sort() to change your list so that it is stored in reverse 
+#       alphabetical order. Print the list to show that the order has been
+#       changed.
+#
+#Write your code below:
