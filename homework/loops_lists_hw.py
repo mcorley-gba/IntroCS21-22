54224006260a2e19f1eaa230fb9bc3d9b729cec8
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-09-26 20:59:46 -0400

    New in-class assignment

---
+++ homework/loops_lists_hw.py
@@ -0,0 +1,31 @@
+#Problem 1:
+#   1.) Think of at least three kinds of your favorite pizza. Store these pizza names in a list.
+#       Use a for loop to print the name of each pizza
+#   2.) Make a new for loop that will print a sentence using the name of the pizza,
+#       rather than just printing the name only. For each pizza, you should have one
+#       line of output containing a simple statement.
+#       Ex: 'I like pepperoni pizza.'
+#   3.) Add a line outside of the for loop for part 2 which states how much you like pizza.
+#       The output should now have one additional sentence.
+#       Ex: 'Pizza is my favorite food.'
+#Write your code belo:
+
+
+
+
+
+#Problem 2:
+#   1.) Use a for loop to print the numbers from one to twenty.
+#   2.) Use the range() function to make a list of odd numbers
+#       from one to one-million. Use a for loop to print the numbers.
+#       (Hint: To get only odd numbers, you will have to use the third
+#       argument of the range() function.)
+#   3.) Make an empty list called 'cubes'. Use a for loop to append
+#       the first 10 cubes to this list in order. 
+#       (Hint: You will have to use the ** operator to get the cubes.)
+#Write your code below:
+
+
+
+
+
