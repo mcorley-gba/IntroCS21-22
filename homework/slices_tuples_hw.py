cc2e266a05c42f0a777d7fa4fea78daef124e8e4
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-10-13 05:13:43 -0400

    added slices_tuples_hw

---
+++ homework/slices_tuples_hw.py
@@ -0,0 +1,28 @@
+#The Problems below are about slices and tuples. Feel free to 
+#use your notes or collaborate with classmates. Turn in your hw
+#to google classroom.
+
+
+#Problem 1:
+#1. Create a list with at least five different kinds of pizza. 
+#   Call this list my_pizzas. 
+#2. Use a slice to print the middle 3 pizzas. 
+#3. Make a completely separate copy of the list. Call this list 
+#   friend_pizzas
+#4. Add a new pizza to my_pizzas. 
+#5. Add a new pizza to friend_pizzas.
+#6. Print both pizza lists to show that they are indeed distinct. 
+#Your code goes here:
+
+
+
+
+#Problem 2:
+#A buffet restaurant has five basic foods. Think of five simple 
+#and store them in a tuple. 
+#1. Use a for loop to print each food in the tuple. 
+#2. The restaurant changes two of its food options. 
+#   Replace two foods in your tuple with different foods. 
+#Your code goes here:
+
+
