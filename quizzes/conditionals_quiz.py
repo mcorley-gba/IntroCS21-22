3d68bab8bf226303ab6ef23e69e285732353bedf
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-10-31 21:21:34 -0400

    Added Quiz over Conditionals

---
+++ quizzes/conditionals_quiz.py
@@ -0,0 +1,22 @@
+#This quiz is over the major topics of "conditionals"
+#When you have finished, save your work as 
+#       [yourname]_conditionals_quiz.py
+#and submit in Google Classroom.
+
+
+#Below is a list of 5 usernames on the supercomputer
+#at UTC known as qbert. The list includeds an 'admin' user. 
+#Loop through the list of usernames and print a welcome 
+#message to each:
+#   *If the username is 'admin', print a special greeting
+#    such as 'Hello, admin, would you like to see a system status report?'
+#
+#   *Otherwise, print a generic greeting such as 
+#    'Hello, Jim, thank you for logging in again.'
+#BONUS POINTS:
+# 2 pts: Print your messages using f-strings.
+# 5 pts: Check to make sure the username list is nonempty.
+#        If it is empty, print an error message:
+#           'Error: No Registered Users. Contact System Administrator.'
+users = ['admin', 'froosevelt', 'mcorley', 'gpatton', 'wchurchill']
+#Your code goes below:
