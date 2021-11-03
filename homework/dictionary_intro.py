8572a815b296d0b596f5e3678ce33938e1c16473
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-11-03 11:41:24 -0400

    Added Dictionary Assignment

---
+++ homework/dictionary_intro.py
@@ -0,0 +1,17 @@
+#A Python dictionary can be used to model an actual dicitonary.
+#To avoid confusion, let's call the actual dictionary a Glossary instead.
+
+#Below, you will make a glossary of 5 programming words that we have learned
+#about so far in this semester.
+
+#1. Think of five programming words that we have learned about and discussed
+#   this semester. Use these words as keys in a python dictionary. Store their meanings
+#   as the values (put the meanings in your own words, you do not need to look up precise or
+#   technical meanings.)
+
+#2. Print each word and it's meaning neatly formatted, one at a time. Some suggestions:
+#   You could print the word then a colon, then the definition all on one line,
+#   or you could print the word on one line, then the definition on a new line.
+#   BONUS: Do these prints using a for loop through the dictionary.
+
+#Your code goes below here:
