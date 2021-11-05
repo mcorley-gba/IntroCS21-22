644084fbf97ce88b6ad047d13f94980850390b43
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-11-05 13:24:25 -0400

    Dictionary Loop More Notes

--- notes/Dictionaries/dictionary_loops.py
+++ notes/Dictionaries/dictionary_loops.py
@@ -26,7 +26,62 @@ favorite_classes = {
     'Doug'      :'Math',
     'Noah L'    :'Computer Science',
     'Logan'     :'Bible',
-    'MaKenna'   :'Biology'}
-
+    'MaKenna'   :'Biology'
+}
+#   keys    values
 for name, course in favorite_classes.items():
-    print(f"{name.title()}'s favorite class is {course.title()}.")
+    print(f"{name}'s favorite class is {course}.")
+
+print("\n\n\n\n\n\n\n\n")
+
+#Loop only through the keys in a dictionary
+for name in favorite_classes.keys(): #.keys() -- creates a list of the keys from the dictionary.
+    print(name)
+
+""" for dummy_var in favorite_classes: #Looping through keys is the default behavior 
+#                               when looping through a dictionary
+    print(dummy_var) """
+
+#Accessing/Looping through the values in a dictionary
+for name in favorite_classes.keys():
+    print(favorite_classes[name])
+
+for course in favorite_classes.values():
+    print(course)
+
+print("\n\n\n\n\n\n\n\n\n")
+
+#Looping through specific/subset of keys in a dictionary:
+#Create a list of the keys that we want:
+state_xc = ['Ty', 'Noah J', 'Collin']
+
+#2 Ways:
+#1: Loop through all keys, use conditionals to filter:
+for name in favorite_classes.keys():
+    if name in state_xc:
+        print(f'{name} once said, "My favorite class is {favorite_classes[name]}"')
+
+print("\n\n\n\n\n")
+
+#2: Loop through the list and use .get()
+for name in state_xc:
+    favorite_course = favorite_classes[name]           #.get(name, f"{name} not in list")
+    print(favorite_course)
+
+
+print("\n\n\n\n\n\n")
+
+for course in favorite_classes.values():
+    print(course)
+
+print("\n\n\n\n\n\n")
+#A set in python is a collection of values in which each item is/must be unique
+
+#2 ways to make a set:
+#1: Call the set function around a list:
+for course in set(favorite_classes.values()):
+    print(course)
+
+#2: define a brand-new set:
+courses = {'Biology', 'Chem', 'Math', 'CS', 'Chem', 'Bible', 'PE', 'Math'}
+print(courses)
