76d0f1c47b0a3bb611b4a1831f4e159fc91ea7e4
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-11-07 16:04:27 -0500

    Added dictionary+loops classwork.

---
+++ homework/dictionary_loops_hw.py
@@ -0,0 +1,11 @@
+#Make a python dictionary containing three major rivers and the country
+#each runs through. For example, one key-value pair might be 'nile':'egypt'
+
+#Use a loop to print a sentence about each river. For example,
+#'The Nile runs through Egypt.' -- Note the proper capitalization. 
+
+#Use another loop to print he name of each river in the dictionary.
+
+#Use a third loop to print the name of each country included in the dictionary.
+
+#Your code goes here:
