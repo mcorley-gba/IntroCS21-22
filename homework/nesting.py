e10652a3d0bd2b6f7b3a379e813d94a39186aaf0
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-11-10 15:39:38 -0500

    Added classwork on Nesting

---
+++ homework/nesting.py
@@ -0,0 +1,10 @@
+#In this exercise you will create a dictionary containing lists as its values:
+#
+#Create a python dictionary using the names of three of your CS classmates as the keys
+#Ask each classmate what their three favorite places are.
+#Store these favorite places in separate lists as the values of the dicitonary.
+#EXAMPLE: 'Dr. Corley':['Chattanooga', 'Disney World', 'Grace Academy']
+#
+#After creating your dictionary, loop through the dictionary and print each persons name
+#and their favorite places. There are mahy ways to format this, think about what seems best to you.
+#Try to use formatting other than basic prints.
