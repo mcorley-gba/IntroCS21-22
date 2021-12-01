6469f2301b4bd01d8f411257127ae3626cf48d77
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-12-01 09:52:52 -0500

    While Loops

---
+++ quizzes/while_loop_quiz.py
@@ -0,0 +1,39 @@
+#PROBLEM 1:
+
+#You work for a sandwich shop. Below is a list of sandwich orders that need to be completed
+#and an empty list for sandwiches that have been completed.
+#Write code that will ...
+#   1.) Loop though the unfinished list and 
+#       a.)print a message saying "We are making your <sandwich>."
+#       b.) Add the current sandwich to the completed list
+#       c.) Delete the current sandwich from the uncompleted list.
+#   2.) When all sandwiches have been made, print a message saying that all sandwiches are complete.
+#When your code is finished, unfinished_sandwiches should be empty and completed_sandwiches should be nonempty
+
+#List of unfinished sandwiches and empty list of finished sandwiches:
+unfinished_sandwiches = ['tuna salad', 'ham and cheese', 'meatball sub', 'roast beef']
+finished_sandwiches = []
+#YOUR CODE GOES BELOW:
+
+
+
+
+
+
+
+
+
+#PROBLEM 2:
+
+#You are working for a travel agency collecting data on popular vacation responses. 
+#Below is an empty list called vacation_spots.
+#Write code that will...
+#   1.) Ask users to input a vacation destination they would like to visit
+#   2.) Record their response in the list
+#   3.) Ask the user if anyone else needs to take the survey
+#   4.) Repeat 1-3 until the user says there are no more respondents.
+#HINT: For this loop, you will want to use a flag or user-input (e.g., 'quit') as the exit condition.
+
+#Empty vacation destination list:
+vacation_spots = []
+#YOUR CODE GOES BELOW:
