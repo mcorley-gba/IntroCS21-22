e3de58bc5050fe5d5aa298f8c8c4f12a169f82ed
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-12-08 08:14:02 -0500

    intro functions classwork

---
+++ homework/intro_functions.py
@@ -0,0 +1,20 @@
+#In this assignment you will write two functions. Your functions should not make any print statements. 
+#Any printing should be done by driver code outside the functions.
+
+#Problem 1:
+#Write a function tha will take two parameters: city and country. You can name it whatever you want.
+#The function should return a formatted string in the form 'City, Country'. 
+#For example, if city='sanTiago' and country='ChiLE', then the function should return the string 
+# 'Santiago, Chile'. 
+#Remember, any printing should be done from the driver code, not within the function.
+#YOUR CODE GOES HERE:
+
+
+
+
+
+#Problem 2:
+#Write a function called double_sum which will take two parameters num1 and num2 and will return 
+# twice the sum of the numbers. For example, double_sum(3,5) should return 16. 
+#As in the first problem. The function not do any printing. 
+#YOUR CODE GOES HERE:
