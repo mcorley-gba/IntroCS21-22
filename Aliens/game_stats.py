cc2ebf3e429f3f3eddbe92e822c330306c6a675a
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2021-12-15 20:18:37 -0500

    GameSample

---
+++ Aliens/game_stats.py
@@ -0,0 +1,18 @@
+class GameStats:
+    """Track statistics for alien invasion"""
+
+    def __init__(self, ai_game):
+        """Initialize Statistics"""
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+        #Start game in inactive state
+        self.game_active = False
+
+        #Start in active state
+        self.game_active = True
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.settings.ship_limit
+        self.score = 0
---
+++ Aliens/settings.py
@@ -0,0 +1,44 @@
+class Settings:
+    """A class for Alien Invasion Settings"""
+
+    def __init__(self):
+        """Initialize game's static settings"""
+        #Screen Settings:
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+
+        #Ship Settings
+        self.ship_limit = 3
+
+        #Bullet Settings:
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        self.bullets_allowed = 3
+
+        #Alien Settings:
+        self.fleet_drop_speed = 10
+
+        #How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        #Fleet Direction: 1=right; -1=left
+        self.fleet_direction = 1
+
+        #Scoring
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Increase speed settings"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
