bd5d0829482857d9fd724831592284bf528c80c0
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2022-01-20 10:44:13 -0500

    Solution to intro classes HW

--- homework/intro_classes_hw.py
+++ homework/intro_classes_hw.py
@@ -5,10 +5,36 @@
 #stating that the restaurant is open.
 #YOUR CODE GOES BELOW:
 
+class Restaurant:
+    """Class to simulate a restaurant. Methods to describe and open the restaurant."""
 
+    def __init__(self,restaurant_name, cuisine_type):
+        """Initialization for Restaurant. Stores name and type of cuisine."""
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
 
+    def describe_restaurant(self):
+        """Prints a statement about the restaurant"""
+        print(f"\nThe name of my restaurant is {self.restaurant_name.title()}.")
+        print(f"{self.restaurant_name.title()} serves {self.cuisine_type.title()} food.")
+
+    def open_restaurant(self):
+        """Simulating opening the restaurant by printing a message saying the restaurant is open."""
+        print(f"{self.restaurant_name.title()} is open!")
 
 
 
 #After making your class, create three different instances of the class.
 #Call describe_restaurant() for each instance.
+the_olive_garden = Restaurant("The Olive Garden", "italian")
+
+restaurant_1 = Restaurant("The Cheesecake Factory", "American")
+restaurant_1.describe_restaurant()
+
+amigos = Restaurant("Amigo's", "mexican")
+amigos.describe_restaurant()
+
+mcdonalds = Restaurant("McDonald's", "American")
+mcdonalds.describe_restaurant()
+mcdonalds.open_restaurant()
+# !=
