01dcab413eaec46a8f2e125fb6004fb27a291375
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2022-01-20 08:10:28 -0500

    intro hw for classes

---
+++ homework/intro_classes_hw.py
@@ -0,0 +1,14 @@
+#Make a class called Restaurant. The initialization method for Restaurant
+#should store two attributes: restaurant_name and cuisine_type. Make a method
+#within the class called describe_restaurant() that prints these pieces of
+#information. Make a second method called open_restaurant() that prints a message
+#stating that the restaurant is open.
+#YOUR CODE GOES BELOW:
+
+
+
+
+
+
+#After making your class, create three different instances of the class.
+#Call describe_restaurant() for each instance.
