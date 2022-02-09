40eae28817898f9030513c913fa4f205060c4c22
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2022-02-09 07:56:49 -0500

    Edits on test on classes

--- tests/classes_test.py
+++ tests/classes_test.py
@@ -3,26 +3,27 @@
 #    a)The __init__() method for this class should take in two parameters
 #      (other than 'self') -- restaurant_name and cuisine_type -- and store
 #      them as attributes.
-#    b)The __init__() method should also create an attribute called
+#    b)The __init__() method should also create (NOT take-in from the user) an attribute called
 #      number_served. This attribute should be initialized to zero.
-#    c)Add a method to Restaurant called describe_restaurant() which prints
+#    c)Add a method to Restaurant called describe_restaurant() which takes no parameters prints
 #      the restaurant name and the cuisine type.
-#    d)Add a method called open_restaurant() that prints a message stating
+#    d)Add a method called open_restaurant() that takes no parameters and prints a message stating
 #      that the restaurant is open.
-#    e)Add a method called set_number_served(...) that takes a number as
+#    e)Add a method called set_number_served(...) that takes a number as a parameter
 #      and sets that as the number_served attribute.
-#    f)Add a method called increment_number_served() that increments the
+#    f)Add a method called increment_number_served() that takes no parameters and increments the
 #      number_served attribute by 1 each time it is called.
 #
 #
 #Problem 2:
 #Create a class called IceCreamStand that is a child of Restaurant.
-#    a)Initialize IceCreamStand using the super().__init__ call.
+#    a)Initialize IceCreamStand using the super().__init__() call.
 #    b)Create a new class called IceCreamFlavors that has the following
 #      features.
 #        1)Stores a list of three available flavors as an attribute in the
-#          __init__() method.
-#        2)Has a method called flavors_today() that prints a message reporting
+#          __init__() method. NOTE: The list should be a python list and it 
+#          SHOULD NOT be a parameter in the __init__ for IceCreamStand.
+#        2)Has a method called flavors_today() that takes no parameters and prints a message reporting
 #          the three flavors available.
 #    c)Add an attribute to the IceCreamStand __init__() method called
 #      available_flavors. This attribute should be an instance of the
