804e8f8e0a84ca3d9dbf62b3285a102922edf581
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2022-04-08 13:26:39 -0400

    Created the Fleet of Aliens

--- AlienInvasion/alien_invasion.py
+++ AlienInvasion/alien_invasion.py
@@ -123,6 +123,27 @@ class AlienInvasion:
         Group abilities"""
         #Make one alien and add it to the group of aliens
         alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2*alien_width)
+        number_aliens_x = available_space_x // (2*alien_width)
+
+        #Determine the number of rows that will fit.
+        ship_height = self.ship.rect.height
+        available_space_y = self.settings.screen_height - (3*alien_height) - ship_height
+        number_rows = available_space_y //(2*alien_height)
+
+        #Create the fleet
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number,row_number):
+        #create an alien and place in the new row.
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + (2*alien_width*alien_number)
+        alien.rect.x = alien.x
+        alien.rect.y = alien_height + (2*alien.rect.height*row_number)
         self.aliens.add(alien)
 
 
