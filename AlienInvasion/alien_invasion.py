a2b46f328b89e244107a6f016090d0334eec043e
mcorley-gba <89784512+mcorley-gba@users.noreply.github.com>
2022-04-29 13:23:08 -0400

    Play Button Added (incomplete)

--- AlienInvasion/alien_invasion.py
+++ AlienInvasion/alien_invasion.py
@@ -116,6 +116,9 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP: #Check if a key has been release
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
     
     def _check_keydown_events(self,event):
         """Respond to keypresses"""
@@ -136,6 +139,11 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when player clicks the 'Play' button"""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the 
